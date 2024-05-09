af1c290e6d433348067303b8dd989a9c6624d63a
reedycat <wpsells@gmail.com>
2024-05-09 14:12:29 +0300

    initial commit

--- load-context.ts
+++ load-context.ts
@@ -1,4 +1,4 @@
-import { type PlatformProxy } from "wrangler";
+import { type PlatformProxy } from 'wrangler'
 
 // When using `wrangler.toml` to configure bindings,
 // `wrangler types` will generate types for those bindings
@@ -8,10 +8,30 @@ import { type PlatformProxy } from "wrangler";
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Env {}
 
-type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
+type Cloudflare = Omit<PlatformProxy<Env>, 'dispose'>
+type LoadContext = {
+	cloudflare: Cloudflare
+}
 
-declare module "@remix-run/cloudflare" {
-  interface AppLoadContext {
-    cloudflare: Cloudflare;
-  }
+declare module '@remix-run/cloudflare' {
+	interface AppLoadContext {
+		env: Cloudflare['env']
+		cf: Cloudflare['cf']
+		ctx: Cloudflare['ctx']
+		cache: Cloudflare['caches']
+	}
+}
+
+export function getLoadContext({
+	context,
+}: {
+	request: Request
+	context: LoadContext
+}) {
+	return {
+		env: context.cloudflare.env,
+		cf: context.cloudflare.cf,
+		ctx: context.cloudflare.ctx,
+		cache: context.cloudflare.caches,
+	}
 }
---
+++ remix.config.js
@@ -0,0 +1,17 @@
+import { flatRoutes } from 'remix-flat-routes'
+/** @type {import('@remix-run/dev/dist/vite/plugin').VitePluginConfig} */
+export default {
+	ignoredRouteFiles: ['**/*'],
+	serverModuleFormat: 'esm',
+	serverDependenciesToBundle: [/^remix-utils.*/],
+	future: {
+		v3_fetcherPersist: true,
+		v3_relativeSplatPath: true,
+		v3_throwAbortReason: true,
+	},
+	routes: async (defineRoutes) => {
+		return flatRoutes('routes', defineRoutes, {
+			ignoredRouteFiles: ['**/*.test.{js,jsx,ts,tsx}', '**/__*.*'],
+		})
+	},
+}
